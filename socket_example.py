8bfbfe3b2996a0ae873ffba5330fa30839963b61
david <davissoftware6@gmail.com>
2023-03-14 13:01:46 +0100

    implemented socket example

--- socket_example.py
+++ socket_example.py
@@ -1,14 +1,11 @@
-from enc_dec import full_encrypt, full_decrypt, public_keys
+from enc_dec import full_encrypt, full_decrypt
 from keytools import load_private_key, load_public_key
 
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 
 import socket
-
-
-addr = input("Address >")
-port = int(input("Port >"))
-name = input("Name >")
+import threading
 
 
 def export_key(key):
@@ -16,13 +13,56 @@ def export_key(key):
                             format=serialization.PublicFormat.SubjectPublicKeyInfo)
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((addr, port))
-    print("Connected")
+def import_key(key):
+    return serialization.load_pem_public_key(key, backend=default_backend())
 
+
+def receiver(conn, private_key, other_public_key):
+    while True:
+        enc_data = conn.recv(1024)
+        if not enc_data:
+            break
+
+        enc_data, sig = enc_data.split(b"|||")
+        data = full_decrypt(enc_data, sig, private_key, other_public_key)
+        print("\n" + data.decode("utf-8"))
+
+
+def main(addr, port):
     private_key = load_private_key("private_key.pem")
     public_key = load_public_key("public_key.pem")
-    receiver_keys = public_keys("public_keys")
 
-    sock.send(name.encode("utf-8"))
-    sock.send(export_key(public_key))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((addr, port))
+
+        sock.listen(1)
+        conn, usr = sock.accept()
+        print("Connected")
+
+        conn.sendall(export_key(public_key))
+        print("Sent public key")
+
+        other_public_key = import_key(conn.recv(1024))
+        print("Received public key")
+
+        receiver_thread = threading.Thread(target=receiver, args=(conn, private_key, other_public_key,))
+        receiver_thread.start()
+
+        while True:
+            try:
+                data = input("\nMessage >")
+                if data == "exit":
+                    break
+
+                enc_data, sig = full_encrypt(data, private_key, other_public_key)
+                conn.sendto(enc_data + b"|||" + sig, usr)
+            except KeyboardInterrupt:
+                conn.close()
+                break
+
+
+if __name__ == '__main__':
+    main(
+        input("Address >"),
+        int(input("Port >"))
+    )
